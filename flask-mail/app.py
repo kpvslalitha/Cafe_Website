b9b6e17b54992e22da94a05c19ff6448800b6668
kpvslalitha <kpvs.lalitha@gmail.com>
2025-04-11 18:16:00 +0530

    Test automatic build - Fix CORS issue

--- flask-mail/app.py
+++ flask-mail/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import smtplib
 from email.message import EmailMessage
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/send-email', methods=['POST'])
 def send_email():
